41fffa4cbbd98015020122bc303f1556cd2415f4
TheKingZirush <zirush42@gmail.com>
2025-01-06 21:38:55 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,4 +2,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 2luUZX8239DiHvg4tSx1TJ79JSY_3ihh1ixZ4MhReKtEUmmKM
-./ngrok http --url=related-aphid-certain.ngrok-free.app 80
+./ngrok http --url=related-aphid-certain.ngrok-free.app 4080
